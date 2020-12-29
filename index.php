88cc4c49c98256546f3e2678b2dc7a8624423350
jason-michael <michael.jason@outlook.com>
2020-12-28 23:36:43 -0600

    add php contact script

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
+<?php   
+    $msg = '';          # The message to show when the form has been submitted.
+    $msgClass = '';     # The class to apply to the message element on submit.
+    $errorClass = '';   # The class to use on error.   //TODO: decide classname and update scss.
+    $successClass = ''; # The class to use on success. //TODO: decide classname and update scss.
+
+    $emailRecipient = 'jason@redsundigitalkc.com'; # Who receives the contact request.
+
+    # On submit:
+    if(filter_has_var(INPUT_POST, 'submit')) {
+        # Get form data
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
+
+        # Check for empty required fields:
+        if(!empty($email) && !empty($name) && !empty($message)) {            
+
+            # Validate email:
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                # Email entered is INVALID.
+                $msg = 'Invalid email.';
+                $msgClass = $errorClass;
+            } else {
+                # Email entered is VALID.
+                $toEmail = $emailRecipient;
+                $subject = 'Contact Request From '.$name;
+                $body = 
+                    '<h2>Contact Request</h2>
+                    <h4>Name</h4> <p>'.$name.'</p>
+                    <h4>Email</h4> <p>'.$email.'</p>
+                    <h4>Message</h4> <p>'.$message.'</p>';
+
+                $headers = "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-Type:text/html;charset=UTF-8;"."\r\n";
+                $headers .= 'From: '.$name.'<'.$email.'>'."\r\n";
+
+                # Send the email:
+                if (mail($toEmail, $subject, $body, $headers)) {
+                    # Email success
+                    $msg = 'Your contact request has been sent.';
+                    $msgClass = $successClass;
+                } else {
+                    # Email failed
+                    $msg = 'Your contact request was not sent.';
+                    $msgClass = $errorClass;
+                }
+            }
+        } else {
+            # A required field was not entered.
+            $msg = 'Please fill in all fields.';
+            $msgClass = $errorClass;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -227,10 +283,16 @@
       <h1>Contact</h1>
       <h3>Shoot us a message and we'll get back to you as quickly as possible.</h3>
     </div>
-    <form action="contact.php" method="POST">
-      <input type="text" name="visitor_name" placeholder="Name">
-      <input type="email" name="visitor_email" id="email" placeholder="Email" required pattern=".+@.+\..+">
-      <textarea id="message" name="visitor_message" placeholder="Message" rows="3" required></textarea>
+    <?php if($msg != ''): ?>
+        <!-- TODO: position and style this element. -->
+        <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+        <?php endif; ?>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <input type="text" name="name" placeholder="Name" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
+      <input type="email" name="email" id="email" placeholder="Email" required pattern=".+@.+\..+" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+      <textarea id="message" name="message" placeholder="Message" rows="3" required>
+        <?php echo isset($_POST['message']) ? $message : ''; ?>
+      </textarea>
       <button type="submit" id="contact-send" class="call-to-action">Send</button>
     </form>
   </section>
